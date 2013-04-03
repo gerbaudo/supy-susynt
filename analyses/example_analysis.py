51eb0d079de8c90b5be2b127b1046ec0e4215f5a
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-04-02 17:47:17 -0700

    cleanup, use only mc

--- analyses/example_analysis.py
+++ analyses/example_analysis.py
@@ -14,36 +14,23 @@ class example_analysis(supy.analysis):
                 )
 
     def listOfSampleDictionaries(self):
-        dir = "/afs/cern.ch/user/e/elaird/public/susypvt/framework_take3/"
-        dir = '/tmp/gerbaudo/supy-minimal-example/data/'
+        dir = '/gdata/atlas/ucintprod/SusyNt/susy_n0135'
         holder = supy.samples.SampleHolder()
-
-        holder.add("Data",
-#                   '["%s/skimmed_900_GeV_Data.root"]' % dir,
-                   '["/gdata/atlas/ucintprod/SusyNt/susy_n0135/user.zgecse.mc12_8TeV.176584.Herwigpp_simplifiedModel_wA_noslep_WH_2Lep_11.SusyNt.e1702_s1581_s1586_r3658_r3549_p1328_n0135/user.zgecse.026706._00001.susyNt.root"]',
-                   lumi=1.0e-5,  # /pb
-                   )
-        holder.add("MC",
-                   '["%s/skimmed_900_GeV_MC.root"]' % dir,
-                   xs=1.0e8,  # pb
+        holder.add("WH_2Lep_176584",
+                   '["%s/user.zgecse.mc12_8TeV.176584.Herwigpp_simplifiedModel_wA_noslep_WH_2Lep_11.SusyNt.e1702_s1581_s1586_r3658_r3549_p1328_n0135/user.zgecse.026706._00001.susyNt.root"]'%dir,
+                   xs=1.140,  # /pb
                    )
         return [holder]
 
     def listOfSamples(self, config):
-        return (supy.samples.specify(names="Data",
-                                     color=r.kBlack,
-                                     markerStyle=20)
- #                supy.samples.specify(names="MC",
-#                                      color=r.kRed,
-#                                      effectiveLumi=0.5)
+        return (supy.samples.specify(names="WH_2Lep_176584",
+                                     color=r.kBlack)
                 )
 
     def conclude(self, pars):
         #make a pdf file with plots from the histograms created above
         org = self.organizer(pars)
-        org.scale()
+        org.scale(lumiToUseInAbsenceOfData=20.0)
         supy.plotter(org,
                      pdfFileName=self.pdfFileName(org.tag),
- #                     samplesForRatios=("Data", "MC"),
-#                      sampleLabelsForRatios=("data", "sim"),
                      ).plotAll()
