a8bb79b7490529b09a98340b21024a3a83c09b57
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-04-02 17:40:53 -0700

    use n0135 ntuple

--- analyses/example_analysis.py
+++ analyses/example_analysis.py
@@ -5,10 +5,7 @@ import ROOT as r
 class example_analysis(supy.analysis):
     def listOfSteps(self, config):
         return [supy.steps.printer.progressPrinter(),
- #                supy.steps.histos.value('run', 20, 0, 1e4),
-#                 supy.steps.histos.value('bx', 3564, 0., 3564.),
                 supy.steps.histos.value('Two', 10, 0, 10),
-                supy.steps.printer.printstuff(['event'])
                 ]
 
     def listOfCalculables(self, config):
@@ -23,7 +20,7 @@ class example_analysis(supy.analysis):
 
         holder.add("Data",
 #                   '["%s/skimmed_900_GeV_Data.root"]' % dir,
-                   '["/gdata/atlas/ucintprod/SusyNt/susy_n0115/user.sfarrell.mc12_8TeV.176584.Herwigpp_simplifiedModel_wA_noslep_WH_2Lep_11.SusyNt.e1702_s1581_s1586_r3658_r3549_p1181_n0115/user.sfarrell.096947._00001.susyNt.root"]',
+                   '["/gdata/atlas/ucintprod/SusyNt/susy_n0135/user.zgecse.mc12_8TeV.176584.Herwigpp_simplifiedModel_wA_noslep_WH_2Lep_11.SusyNt.e1702_s1581_s1586_r3658_r3549_p1328_n0135/user.zgecse.026706._00001.susyNt.root"]',
                    lumi=1.0e-5,  # /pb
                    )
         holder.add("MC",
