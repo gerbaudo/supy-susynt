b95ec0f8e04791ec7362ac55232a7c57dcf5aa1c
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-04-08 16:15:13 -0700

    switch to the usual collection syntax(pair)

--- calculables/dilepton.py
+++ calculables/dilepton.py
@@ -9,37 +9,37 @@ class signalLeptons(supy.wrappedChain.calculable) :
         self.value = sorted([e for e in el] + [m for m in mu], key=lambda x: x.Pt(), reverse=True)
 #___________________________________________________________
 class sameSign(supy.wrappedChain.calculable) :
-    def __init__(self, collection = 'signalLeptons'):
-        self.coll = collection
+    def __init__(self, collection = ('signalLeptons','')):
+        self.coll = collection[0]
     def update(self, _) :
         leptons = self.source[self.coll]
         self.value = False if len(leptons)<2 else leptons[0].q*leptons[1].q > 0
 #___________________________________________________________
-class oppositeSign(supy.wrappedChain.calculable) :
+class oppositeSign(sameSign) :
     def update(self, _) :
         self.value = not self.source['sameSign']
 #___________________________________________________________
 class sameFlavor(supy.wrappedChain.calculable) :
-    def __init__(self, collection = 'signalLeptons'):
-        self.coll = collection
+    def __init__(self, collection = ('signalLeptons','')):
+        self.coll = collection[0]
     def update(self, _) :
         leptons = self.source[self.coll]
         self.value = False if len(leptons)<2 else leptons[0].isMu()==leptons[1].isMu()
 #___________________________________________________________
-class oppositeFlavor(supy.wrappedChain.calculable) :
+class oppositeFlavor(sameFlavor) :
     def update(self, _) :
         self.value = not self.source['sameFlavor']
 #___________________________________________________________
 class diElectron(supy.wrappedChain.calculable) :
-    def __init__(self, collection = 'signalLeptons'):
-        self.coll = collection
+    def __init__(self, collection = ('signalLeptons','')):
+        self.coll = collection[0]
     def update(self, _) :
         leptons = self.source[self.coll]
         self.value = False if len(leptons)<2 else leptons[0].isEle() and leptons[1].isEle()
 #___________________________________________________________
 class diMuon(supy.wrappedChain.calculable) :
-    def __init__(self, collection = 'signalLeptons'):
-        self.coll = collection
+    def __init__(self, collection = ('signalLeptons','')):
+        self.coll = collection[0]
     def update(self, _) :
         leptons = self.source[self.coll]
         self.value = False if len(leptons)<2 else leptons[0].isMu() and leptons[1].isMu()
