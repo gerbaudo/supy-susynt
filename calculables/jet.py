942e678bebead03954f689245e05fa95af83613e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-04-11 14:30:45 -0700

    add true jets calcs

--- calculables/jet.py
+++ calculables/jet.py
@@ -6,7 +6,6 @@ MeV2GeV=0.001
 
 #___________________________________________________________
 class signalJets(supy.wrappedChain.calculable) :
-
     def __init__(self):
         maxEta, minPt, minJvf = 2.5, 20., 0.5
         self.maxEta = maxEta
@@ -20,8 +19,25 @@ class signalJets(supy.wrappedChain.calculable) :
         self.value = [j for j in self.source['jets']
                       if abs(j.Eta())<self.maxEta and j.Pt()>self.minPt and \
                       (j.jvf < 0.0 or j.jvf>self.minJvf)]
-
+#___________________________________________________________
 class indicesSignalJets(supy.wrappedChain.calculable) :
     def update(self, _) :
         self.value = [i for i,jj in enumerate(self.source['signalJets'])]
+#___________________________________________________________
+class trueJets(supy.wrappedChain.calculable) :
+    def __init__(self):
+        maxEta, minPt = 2.5, 20.0
+        self.maxEta = maxEta
+        self.minPt  = minPt
+        self.moreName = ""
+        if maxEta : self.moreName += "|eta|<%.1f"%maxEta
+        if minPt  : self.moreName += "pt>%.1f"%minPt
+    def update(self, _) :
+        self.value = [j for j in self.source['truthJets']
+                      if abs(j.Eta())<self.maxEta and j.Pt()>self.minPt]
+#___________________________________________________________
 
+class indicesTrueJets(supy.wrappedChain.calculable) :
+    def update(self, _) :
+        self.value = [i for i,jj in enumerate(self.source['trueJets'])]
+#___________________________________________________________
