8ca768523428066359c10b86ce68ff81ab23ce16
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-08-18 16:09:51 -0700

    add goodrun and larerr (calc and filter)

--- analyses/cutflow_WZ.py
+++ analyses/cutflow_WZ.py
@@ -3,11 +3,18 @@ import samples
 import steps
 import supy
 import ROOT as r
-
+import os
 
 class cutflow_WZ(supy.analysis):
+    def parameters(self) :
+        return {
+            'grlFile' : os.getenv('ROOTCOREDIR')+'/data/MultiLep/data12_8TeV.periodAllYear_DetStatus-v47-pro13-01_CoolRunQuery-00-04-08_Susy.xml'
+            }
     def listOfSteps(self, config):
+        sf = steps.filters
         return [supy.steps.printer.progressPrinter(),
+                sf.GoodRun().onlyData(),
+                sf.LarErr(),
                 supy.steps.filters.value('sameSign',min=1),
                 supy.steps.histos.multiplicity('signalJets',8),
                 supy.steps.histos.multiplicity('trueJets',8),
@@ -17,9 +24,11 @@ class cutflow_WZ(supy.analysis):
                 ]
 
     def listOfCalculables(self, config):
+        pars = self.parameters()
         cdil = calculables.dilepton
-        cj = calculables.jet
+        ce, cj = calculables.event, calculables.jet
         lcals =  supy.calculables.zeroArgs(supy.calculables)
+        lcals += [ce.Grlt(pars['grlFile']), ce.IsGoodRun(), ce.LarErr()]
         lcals += [cdil.signalLeptons(),
                   cj.signalJets(), cj.indicesSignalJets(),
                   cj.trueJets(), cj.indicesTrueJets(),
@@ -32,12 +41,13 @@ class cutflow_WZ(supy.analysis):
         return [samples.Sherpa_CT10_lllnu_WZ]
 
     def listOfSamples(self, config):
-        return (supy.samples.specify(names='Sherpa_CT10_lllnu_WZ', color=r.kBlack)
+        test = True
+        nEventsMax=1000 if test else None
+        return (supy.samples.specify(names='Sherpa_CT10_lllnu_WZ', color=r.kBlack, nEventsMax=nEventsMax)
                 #+supy.samples.specify(names='WH_2Lep_11_ee', color=r.kBlue)
                 )
 
     def conclude(self, pars):
-        #make a pdf file with plots from the histograms created above
         org = self.organizer(pars)
         org.scale(lumiToUseInAbsenceOfData=20.0)
         supy.plotter(org,
